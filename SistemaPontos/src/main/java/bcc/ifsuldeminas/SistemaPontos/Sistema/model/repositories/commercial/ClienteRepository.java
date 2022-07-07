bf7019f3c5c42c692e2e1a4969bf0720c61a8544
AndreBiajoti <104443456+AndreBiajoti@users.noreply.github.com>
2022-07-06 21:13:45 -0300

    Delete SistemaPontos directory

--- SistemaPontos/src/main/java/bcc/ifsuldeminas/SistemaPontos/Sistema/model/repositories/commercial/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package bcc.ifsuldeminas.SistemaPontos.Sistema.model.repositories.commercial;
-
-import bcc.ifsuldeminas.SistemaPontos.Sistema.model.entity.Usuario.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
--- SistemaPontos/src/main/java/bcc/ifsuldeminas/SistemaPontos/Sistema/model/repositories/commercial/ProdutosRepository.java
+++
@@ -1,7 +0,0 @@
-package bcc.ifsuldeminas.SistemaPontos.Sistema.model.repositories.commercial;
-
-import bcc.ifsuldeminas.SistemaPontos.Sistema.model.entity.Comercial.Produtos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutosRepository extends JpaRepository<Produtos, Long> {
-}
